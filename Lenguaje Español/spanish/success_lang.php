1238f18fb177edd277a4f67cd203b18bc41daf84
Enya Andrea Ribba Hernández <enigmahernandez@gmail.com>
2021-03-02 14:38:31 -0600

    Add files via upload
    Editar el siguiente archivo: config.php
    
    ./application/config/config.php
    
    Cambiar la siguiente línea:
    
    $config['language']	= 'english';
    		
    a la siguiente línea:
    
    $config['language']	= 'español';
    
    
    --------------
    Da error?
    
    Modificar el archivo index.php en ROOT directory 
    Agregar el siguiente código al principio:
    ob_start();
    
    
    ejemplo:
    <?php
    // Descomentar la siguiente línea por si da problemas con
    //ini_set(): Headers already sent. You cannot change the session module's ini settings at this time in codeigniter
    //
    // ob_start(); 
    //

---
+++ Lenguaje Español/spanish/success_lang.php
@@ -0,0 +1,99 @@
+<? php
+// Traducido al español por EnigmaTeam
+// Traducciones del panel de avisos
+$ lang ['success_1'] = "¡Todos los correos electrónicos han sido enviados!";
+$ lang ['success_2'] = "¡El grupo de usuarios fue agregado!";
+$ lang ['success_3'] = "¡El grupo de usuarios fue actualizado!";
+$ lang ['success_4'] = "¡El grupo de usuarios fue eliminado!";
+$ lang ['success_5'] = "¡Los usuarios fueron agregados al grupo!";
+$ lang ['success_6'] = "¡El usuario fue eliminado del grupo!";
+$ lang ['success_7'] = "¡Se actualizó la plantilla de correo electrónico!";
+$ lang ['success_8'] = "¡La IP ha sido agregada a la lista de bloqueo!";
+$ lang ['success_9'] = "¡El bloque de IP fue eliminado!";
+$ lang ['success_10'] = "¡Ha actualizado correctamente la cuenta de usuario!";
+$ lang ['success_11'] = "¡Ha creado una cuenta con éxito!";
+$ lang ['success_12'] = "¡El miembro fue eliminado!";
+$ lang ['success_13'] = "¡Configuración actualizada!";
+$ lang ['success_14'] = "¡Se cambió la configuración de idioma!";
+$ lang ['success_15'] = "¡Ha iniciado sesión con éxito con Google!";
+$ lang ['success_16'] = "¡Has iniciado sesión con éxito usando Facebook!";
+$ lang ['success_17'] = "¡Has iniciado sesión con éxito usando Twitter!";
+$ lang ['success_18'] = "¡Se restableció la contraseña!";
+$ lang ['success_19'] = "¡Se envió un correo electrónico a su cuenta con instrucciones sobre cómo restablecer su contraseña!";
+$ lang ['success_20'] = "¡Ha registrado una cuenta con éxito!";
+$ lang ['success_21'] = "¡Gracias! ¡Su registro ya está completo!";
+$ lang ['success_22'] = "¡Se actualizó su configuración!";
+$ lang ['success_23'] = "¡Su contraseña ha sido cambiada!";
+
+$ lang ['success_24'] = "¡Configuración de pago actualizada!";
+$ lang ['success_25'] = "¡Plan de pago agregado!";
+$ lang ['success_26'] = "¡Plan de pago actualizado!";
+$ lang ['success_27'] = "¡Plan de pago eliminado!";
+$ lang ['success_28'] = "¡Ha comprado el plan, gracias!";
+$ lang ['success_29'] = "¡Necesita una cuenta premium para acceder a esa área del sitio! Elija un plan a continuación.";
+
+$ lang ['success_30'] = "¡Se agregó el rol de usuario!";
+$ lang ['success_31'] = "¡Se actualizó la función del usuario!";
+$ lang ['success_32'] = "¡Se eliminó el rol de usuario!";
+
+$ lang ['success_33'] = "¡Ha registrado una cuenta con éxito! Se ha enviado un correo electrónico a su dirección de correo electrónico que contiene un enlace necesario para hacer clic para activar su cuenta.";
+$ lang ['success_34'] = "¡Gracias por activar su cuenta! ¡Ahora puede iniciar sesión!";
+$ lang ['success_35'] = "Se ha enviado un correo electrónico a la dirección de esa cuenta.";
+
+$ lang ['success_36'] = "Esta cuenta ha sido desactivada y ya no se puede utilizar.";
+$ lang ['success_37'] = "¡El campo se agregó correctamente!";
+$ lang ['success_38'] = "¡El campo se actualizó correctamente!";
+$ lang ['success_39'] = "¡El campo fue eliminado!";
+
+$ lang ['success_40'] = "¡Sus créditos se pagaron con éxito y se agregaron a su cuenta!";
+$ lang ['success_41'] = "¡El anuncio fue agregado!";
+$ lang ['success_42'] = "¡El anuncio fue actualizado!";
+$ lang ['success_43'] = "¡El anuncio fue eliminado!";
+$ lang ['success_44'] = "¡El ticket se creó correctamente!";
+$ lang ['success_45'] = "¡Su respuesta fue publicada!";
+$ lang ['success_46'] = "¡La respuesta se actualizó correctamente!";
+$ lang ['success_47'] = "¡La respuesta fue eliminada!";
+$ lang ['success_48'] = "¡El ticket se actualizó correctamente!";
+$ lang ['success_49'] = "¡El archivo del ticket fue eliminado!";
+$ lang ['success_50'] = "¡El artículo fue actualizado!";
+$ lang ['success_51'] = "¡El artículo fue eliminado!";
+$ lang ['success_52'] = "¡El artículo fue agregado!";
+$ lang ['success_53'] = "¡La categoría se agregó correctamente!";
+$ lang ['success_54'] = "¡La categoría fue eliminada!";
+$ lang ['success_55'] = "¡La categoría fue actualizada!";
+$ lang ['success_56'] = "¡Se le ha asignado el ticket!";
+$ lang ['success_57'] = "¡El ticket fue eliminado!";
+$ lang ['success_58'] = "¡El campo personalizado fue actualizado!";
+$ lang ['success_59'] = "¡Se agregó el campo personalizado!";
+$ lang ['success_60'] = "¡La categoría del ticket fue eliminada!";
+$ lang ['success_61'] = "¡Se agregó la respuesta predefinida!";
+$ lang ['success_62'] = "¡La respuesta predefinida fue actualizada!";
+$ lang ['success_63'] = "¡La respuesta predefinida fue eliminada!";
+
+// v2.1
+$ lang ['success_64'] = "¡El usuario fue asignado al ticket!";
+$ lang ['success_65'] = "¡La vista se configuró como su vista personalizada!";
+$ lang ['success_66'] = "Se actualizó la vista personalizada";
+$ lang ['success_67'] = "Se agregó la vista personalizada";
+$ lang ['success_68'] = "¡La vista personalizada fue eliminada!";
+
+// v2.2
+$ lang ['success_69'] = "¡El cliente fue notificado del ticket!";
+$ lang ['success_70'] = "¡La plantilla de correo electrónico fue eliminada!";
+$ lang ['success_71'] = "¡La plantilla de correo electrónico se agregó correctamente!";
+$ lang ['success_72'] = "¡El ticket se fusionó!";
+
+// V2.4
+$ lang ['success_73'] = "¡Todas las notificaciones se han marcado como leídas!";
+$ lang ['success_74'] = "¡Se agregaron las preguntas frecuentes!";
+$ lang ['success_75'] = "¡Se borraron las preguntas frecuentes!";
+$ lang ['success_76'] = "¡Las preguntas frecuentes fueron actualizadas!";
+$ lang ['success_77'] = "¡Se actualizaron las notas del ticket!";
+$ lang ['success_78'] = "¡Se agregó el estado personalizado!";
+$ lang ['success_79'] = "¡El estado personalizado fue actualizado!";
+$ lang ['success_80'] = "¡El estado personalizado fue eliminado!";
+
+// V2.5
+$ lang ['success_81'] = "¡El documento fue actualizado!";
+$ lang ['success_82'] = "¡El documento fue eliminado!";
+$ lang ['success_83'] = "El archivo fue eliminado
