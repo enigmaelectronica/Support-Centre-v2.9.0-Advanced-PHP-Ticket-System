00ec74f62ecb99cc000f75a4c139237c37a9cc62
Enya Andrea Ribba Hernández <enigmahernandez@gmail.com>
2021-03-03 07:59:45 -0600

    Add files via upload

---
+++ Lenguaje Español/error_lang.php
@@ -0,0 +1,181 @@
+<?php
+//Traduccion al español por EnigmaTeam
+//
+$lang['error_1'] = "¡Debes iniciar sesión para ver esta página!";
+$lang['error_2'] = "¡No tiene acceso a esta página!";
+$lang['error_3'] = "Este usuario no existe:";
+$lang['error_4'] = "¡Grupo no válido!";
+$lang['error_5'] = "¡El nombre del grupo no puede estar vacío!";
+$lang['error_6'] = "¡Hash no válido!";
+$lang['error_7'] = "¡Ese usuario no es parte de este grupo!";
+$lang['error_8'] = "Plantilla de correo electrónico no válida";
+$lang['error_9'] = "¡No puede dejar el título o el mensaje en blanco!";
+$lang['error_10'] = "¡La IP no puede estar vacía!";
+$lang['error_11'] = "Bloque IP no válido";
+$lang['error_12'] = "¡No se pudo encontrar ningún miembro con ese criterio!";
+$lang['error_13'] = "¡Este miembro no existe!";
+$lang['error_14'] = "¡El nombre de usuario debe tener al menos 3 caracteres!";
+$lang['error_15'] = "¡El nombre de usuario solo puede contener letras, números y guiones bajos!";
+$lang['error_16'] = "¡Ese nombre de usuario ya está en uso!";
+$lang['error_17'] = "Su contraseña debe tener al menos 6 caracteres.";
+$lang['error_18'] = "¡El correo electrónico no puede estar vacío!";
+$lang['error_19'] = "¡No ingresó una dirección de correo electrónico válida!";
+$lang['error_20'] = "¡Esa dirección de correo electrónico ya está en uso!";
+$lang['error_21'] = "Error de carga:";
+$lang['error_22'] = "¡Sus contraseñas no coinciden!";
+$lang['error_23'] = "¡El nombre del sitio o el correo electrónico no pueden estar vacíos!";
+$lang['error_24'] = "¡Estadísticas de casa perdidas!";
+$lang['error_25'] = "¡Idioma no válido!";
+$lang['error_26'] = "¡Has sido bloqueado en este sitio!";
+$lang['error_27'] = "¡Ya ha iniciado sesión!";
+$lang['error_28'] = "¡Le faltan algunos detalles!";
+$lang['error_29'] = "¡Detalles de inicio de sesión no válidos!";
+$lang['error_30'] = "¡No puede iniciar sesión usando Socail Services porque ha sido deshabilitado!";
+$lang['error_31'] = "Faltan claves de API de Google";
+$lang['error_32'] = "No se pudo obtener el ID de autenticación de Google";
+$lang['error_33'] = "Faltan claves de API de Facebook";
+$lang['error_34'] = "¡No se puede obtener el token de acceso de FB!";
+$lang['error_35'] = "Faltan claves de API de Twitter";
+$lang['error_36'] = "La configuración de devolución de llamada no se estableció en verdadera. Algo salió mal.";
+$lang['error_37'] = "La API de Twitter no responde. La API de Twitter puede estar inactiva. CÓDIGO HTTP:";
+$lang['error_38'] = "¡Token de Oauth no válido!";
+$lang['error_39'] = "Algo salió mal al obtener el token de acceso de Twitter";
+$lang['error_40'] = "Algo salió mal al verificar sus Credenciales de Twitter";
+$lang['error_41'] = "Requisitos de inicio de sesión no válidos";
+$lang['error_42'] = "¡Detalles de reinicio no válidos!";
+$lang['error_43'] = "¡Token de reinicio caducado!";
+$lang['error_44'] = "¡Sus contraseñas no coinciden!";
+$lang['error_45'] = "¡Su nueva contraseña no puede estar vacía!";
+$lang['error_46'] = "Ya ha solicitado un restablecimiento de contraseña en los últimos 15 minutos";
+$lang['error_47'] = "Dirección de correo electrónico no válida";
+$lang['error_48'] = "¡Falta la plantilla de correo electrónico!";
+$lang['error_49'] = "¡No puedes buscar nada!";
+$lang['error_50'] = "No se pudo encontrar ningún miembro con ese criterio de búsqueda.";
+$lang['error_51'] = "Perfil no válido";
+$lang['error_52'] = "¡Usuario no válido!";
+$lang['error_53'] = "¡Este usuario ha sido baneado!";
+$lang['error_54'] = "¡Registro deshabilitado!";
+$lang['error_55'] = "Captcha no válido";
+$lang['error_56'] = "¡El nombre es demasiado largo!";
+$lang['error_57'] = "¡El apellido es demasiado largo!";
+$lang['error_58'] = "¡El nombre y el apellido no pueden estar vacíos!";
+$lang['error_59'] = "Ingresó la contraseña actual incorrecta";
+$lang['error_60'] = "¡Los pagos han sido deshabilitados!";
+$lang['error_61'] = "¡Plan no válido!";
+$lang['error_62'] = "¡No tiene suficientes créditos para comprar este plan!";
+$lang['error_63'] = "¡Ya tienes el paquete Unlimited Premium Time!";
+$lang['error_64'] = "¡El nombre del rol no puede estar vacío!";
+$lang['error_65'] = "¡El rol de usuario no existe!";
+$lang['error_66'] = "¡Solo un superadministrador puede cambiar el rol de usuario de un miembro!";
+$lang['error_67'] = "Debe ser un usuario superadministrador para poder crear usuarios con privilegios de administrador";
+$lang['error_68'] = "¡Ha intentado iniciar sesión demasiadas veces! Espere 15 minutos antes de volver a intentarlo.";
+$lang['error_69'] = "¡Código no válido!";
+$lang['error_70'] = "Debe esperar 15 minutos antes de enviar otra solicitud de activación por correo electrónico.";
+$lang['error_71'] = "¡Usuario no válido!";
+$lang['error_72'] = "Debe activar su cuenta antes de poder iniciar sesión.";
+$lang['error_73'] = "Haga clic aquí";
+$lang['error_74'] = "para reenviar el código de activación a su dirección de correo electrónico.";
+$lang['error_75'] = "¡El nombre del campo no puede estar vacío!";
+$lang['error_76'] = "Tipo de campo no válido";
+$lang['error_77'] = "¡Campo personalizado no válido!";
+$lang['error_78'] = "Dejó un campo obligatorio vacío:";
+$lang['error_79'] = "¡Pago no válido!";
+$lang['error_80'] = "Su tarjeta ha sido rechazada por la pasarela de pago (STRIPE).";
+$lang['error_81'] = "¡El título no puede estar vacío!";
+$lang['error_82'] = "Anuncio no válido";
+$lang['error_83'] = "¡El título del ticket no puede estar vacío!";
+$lang['error_84'] = "¡Boleto no válido!";
+$lang['error_85'] = "¡No tiene permiso para ver esta área!";
+$lang['error_86'] = "¡Artículo inválido!";
+$lang['error_87'] = "Categoría no válida";
+$lang['error_88'] = "Debe buscar algo";
+$lang['error_89'] = "¡Inicie sesión para ver sus entradas!";
+$lang['error_90'] = "No puede ver un ticket sin ingresar el correo electrónico y la contraseña.";
+$lang['error_91'] = "Detalles de la entrada de invitado no válidos";
+$lang['error_92'] = "¡El cuerpo del ticket no puede estar vacío!";
+$lang['error_93'] = "Prioridad no válida";
+$lang['error_94'] = "No ha introducido un correo electrónico válido para este ticket (invitado)";
+$lang['error_95'] = "Debe iniciar sesión para crear un ticket";
+$lang['error_96'] = "¡Subcategoría no válida!";
+$lang['error_97'] = "Debe seleccionar una subcategoría de esta categoría.";
+$lang['error_98'] = "Hubo un error al cargar el archivo:";
+$lang['error_99'] = "Falta un campo obligatorio:";
+$lang['error_100'] = "¡Su respuesta no puede estar vacía!";
+$lang['error_101'] = "Respuesta no válida";
+$lang['error_102'] = "¡No puede modificar esta respuesta!";
+$lang['error_103'] = "¡La respuesta al ticket no puede estar vacía!";
+$lang['error_104'] = "Categoría principal no válida";
+$lang['error_105'] = "Está intentando seleccionar una subcategoría como categoría principal";
+$lang['error_106'] = "¡No puede editar el ticket!";
+$lang['error_107'] = "Archivo adjunto no válido";
+$lang['error_108'] = "Notificación no válida";
+$lang['error_109'] = "¡El título del artículo no puede estar vacío!";
+$lang['error_110'] = "¡Categoría no válida!";
+$lang['error_111'] = "¡El nombre de la categoría no puede estar vacío!";
+$lang['error_112'] = "¡Este ticket ya tiene un usuario asignado!";
+$lang['error_113'] = "Estado no válido";
+$lang['error_114'] = "¡Nombre de usuario de cliente no válido!";
+$lang['error_115'] = "¡Nombre de usuario asignado no válido!";
+$lang['error_116'] = "No ha especificado un cliente para este ticket (usuario o invitado)";
+$lang['error_117'] = "¡Debe seleccionarse un cliente!";
+$lang['error_118'] = "Campo no válido";
+$lang['error_119'] = "¡El nombre no puede estar vacío!";
+$lang['error_120'] = "¡Grupo no válido!";
+$lang['error_121'] = "¡Categoría principal no válida!";
+$lang['error_122'] = "La categoría no puede ser padre de sí misma";
+$lang['error_123'] = "¡Respuesta inválida!";
+
+// V2.1
+$lang['error_124'] = "La vista no existe";
+$lang['error_125'] = "¡Vista no válida!";
+$lang['error_126'] = "Tipo de pedido no válido";
+
+// V2.2
+$lang['error_127'] = "¡Boleto principal no válido!";
+
+//V2.4
+$lang['error_128'] = "¡Esta sección está deshabilitada!";
+$lang['error_129'] = "¡La pregunta o la respuesta no pueden estar vacías!";
+$lang['error_130'] = "¡Preguntas frecuentes no válidas!";
+$lang['error_131'] = "¡El nombre de usuario no puede estar vacío!";
+$lang['error_132'] = "¡Dejaste el campo de contraseña vacío!";
+$lang['error_133'] = "¡Se debe dar un nombre al estado!";
+$lang['error_134'] = "Estado personalizado no válido";
+
+// 2.5
+$lang['error_135'] = "Proyecto no válido";
+$lang['error_136'] = "¡Documento no válido!";
+$lang['error_137'] = "¡El título del documento no puede estar vacío!";
+$lang['error_138'] = "¡Documento de enlace no válido!";
+$lang['error_139'] = "El documento al que está intentando vincular está vinculado a otro documento. Solo puede vincular a documentos originales.";
+$lang['error_140'] = "¡No puede vincular un documento consigo mismo!";
+$lang['error_141'] = "¡Archivo inválido!";
+$lang['error_142'] = "¡El nombre del proyecto no puede estar vacío!";
+
+// 2.6
+$lang['error_143'] = "¡Este artículo está restringido solo a ciertos miembros!";
+$lang['error_144'] = "¡Ya has expresado tu opinión sobre este artículo!";
+
+// 2.7
+$lang['error_145'] = "No tienes suficientes Créditos para abrir un ticket. Ve a la página de Fondos";
+
+// 2.8
+$lang['error_146'] = "Debe establecer su contraseña en su cuenta antes de poder desautorizar su cuenta de red social.";
+
+// 2.9
+$lang['error_147'] = "¡Google Drive no está autenticado!";
+$lang['error_148'] = "¡Ningún archivo seleccionado!";
+$lang['error_149'] = "¡La carga está inhabilitada!";
+$lang['error_150'] = "";
+$lang['error_151'] = "";
+$lang['error_152'] = "";
+$lang['error_153'] = "";
+$lang['error_154'] = "";
+$lang['error_155'] = "";
+$lang['error_156'] = "";
+$lang['error_157'] = "";
+$lang['error_158'] = "";
+$lang['error_159'] = "";
+
+
+?>
